127d98d95db46b7f307dbced800c3fc9505bf886
Jan <Jan@172.16.138.93>
2014-11-18 15:27:30 -0500

    Day 2.1 stuff, RobotVars completed

--- src/org/team2856/RobotVariables.java
+++ src/org/team2856/RobotVariables.java
@@ -7,4 +7,14 @@ package org.team2856;
  */
 public class RobotVariables {
     
+    //DriveTrain
+    public static final int DT_MC_SLOT = 1,//slot for motor control
+            DT_MC_CHANNEL_LEFT = 1,//channel for left motor control
+            DT_MC_CHANNEL_RIGHT = 2;//channel for right motor control
+    
+    //arm
+    public static final int ARM_SOL_SLOT = 1,//slot for solenoid
+            ARM_SOL_CHANNEL_A = 1,//first channel for solenoid
+            ARM_SOL_CHANNEL_B = 2;//seccond channel for solenoid
+    
 }
